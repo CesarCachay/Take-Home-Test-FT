b36e221cc45e859179ca02ee6fc307d1af07b695
CesarCachay <cesarcachay1996@gmail.com>
2021-01-07 20:02:47 -0500

    Add prettier eslint and husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <div>
-      Starting project!
+      Starting project called take-home-test using React JS and Typescript
     </div>
   );
 };
