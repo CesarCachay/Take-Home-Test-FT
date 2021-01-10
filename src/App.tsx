47ba6d40ba4a43aa96b01b830dfba3823876fd71
CesarCachay <cesarcachay1996@gmail.com>
2021-01-09 19:13:40 -0500

    Add services of github rest api to search and get users

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { getUsers, getUserProfile, searchUsers } from 'services';
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    getUsers();
+    getUserProfile('cesarCachay');
+    searchUsers('cesarCachay');
+  }, []);
+
   return (
     <div>
       Starting project called take-home-test using React JS and Typescript
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,53 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import axios from 'axios';
 
+const baseUrl = 'https://api.github.com';
 const queryAuth = `&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
 
-console.log(axios, queryAuth);
+export const getUsers = () => {
+  try {
+    const response = axios({
+      method: 'get',
+      url: `${baseUrl}/users?since=135?${queryAuth}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error(error.message);
+    return error.response.status;
+  }
+};
+
+export const searchUsers = async (text: string) => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `${baseUrl}/search/users?q=${text}${queryAuth}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error(error.message);
+    return error.response.status;
+  }
+};
+
+export const getUserProfile = async (username: string) => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `${baseUrl}/users/${username}?${queryAuth}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error(error.message);
+    return error.response.status;
+  }
+};
